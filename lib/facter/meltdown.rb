47ac6904bc6dcd19f54a22fd04e20937e5d982de
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2018-01-11 15:57:23 +0100

    pretty printing

--- lib/facter/meltdown.rb
+++ lib/facter/meltdown.rb
@@ -3,9 +3,11 @@ Facter.add('meltdown') do
     value = ""
     checker_script = ""
     setcode do
+      require 'json'
     #    value = Facter::Core::Execution.exec('/var/tmp/spectre-meltdown-check --batch json')
       checker_script = "/opt/puppetlabs/puppet/cache/lib/spectre-meltdown-checker.sh"
       value = Facter::Core::Execution.exec("/bin/sh #{checker_script} --batch json")
+      value = JSON.pretty_generate(value)
     end
     value
 end
