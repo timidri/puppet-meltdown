fa4cd3cc392d7c1a1556469219ceb107273bbcea
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2018-01-11 15:51:57 +0100

    replaced system by Facter::Core::Execution

--- lib/facter/meltdown.rb
+++ lib/facter/meltdown.rb
@@ -5,7 +5,7 @@ Facter.add('meltdown') do
     setcode do
     #    value = Facter::Core::Execution.exec('/var/tmp/spectre-meltdown-check --batch json')
       checker_script = "/opt/puppetlabs/puppet/cache/lib/spectre-meltdown-checker.sh"
-      value = system("/bin/sh", checker_script, "--batch", "json")
+      value = Facter::Core::Execution.exec(("/bin/sh", checker_script, "--batch", "json")
     end
     value
 end
