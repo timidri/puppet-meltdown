19572af2891ca89ea95677a3d8ba89557fd92e29
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-01-11 18:11:36 +0100

    registry settings for feature activition once patched

---
+++ manifests/windows.pp
@@ -0,0 +1,26 @@
+# Class: meltdown::windows
+# Ensures registry settings and OS patches are in place to mitigate against Spectre & Meltdown vulnerabilities
+#
+class meltdown::windows {
+  # resources
+  # These registry values are benign, won't cdo anything on systems that are not yet patched. So always install them.
+  registry_value { 'SpeculativeExecutionProtection_FeatureSettingsOverride':
+    ensure => present,
+    path   => 'HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\FeatureSettingsOverride',
+    type   => dword,
+    data   => 0,
+  }
+  registry_value { 'SpeculativeExecutionProtection_FeatureSettingsOverrideMask':
+    ensure => present,
+    path   => 'HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\FeatureSettingsOverrideMask',
+    type   => dword,
+    data   => 3,
+  }
+  registry_value { 'SpeculativeExecutionProtection_HyperV':
+    ensure => present,
+    path   => 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization\MinVmVersionForCpuBasedMitigations',
+    type   => string,
+    data   => '1.0',
+  }
+
+}
