36dbc94a16d9a25565bb00dbfee28e5a375eab67
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2018-01-12 11:56:06 +0100

    added CVE-specific facts

--- lib/facter/meltdown.rb
+++ lib/facter/meltdown.rb
@@ -1,14 +1,47 @@
+require 'json'
 Facter.add('meltdown') do
-    confine :kernel => :linux
+    # confine :kernel => :linux
     value = ""
     checker_script = ""
     setcode do
-      require 'json'
       # get the script path relative to facter Ruby program
-      checker_script = File.join(File.expand_path(File.dirname(__FILE__)), '..', 
-        'meltdown', 'spectre-meltdown-checker.sh')
-      value = Facter::Core::Execution.exec("/bin/sh #{checker_script} --batch json")
-      value = JSON.pretty_generate(JSON(value))
+      # checker_script = File.join(File.expand_path(File.dirname(__FILE__)), '..', 
+        # 'meltdown', 'spectre-meltdown-checker.sh')
+     value = JSON(Facter::Core::Execution.exec("/bin/sh #{checker_script} --batch json"))
+      # value = <<-EOT
+      # [
+      #   {
+      #     "NAME": "SPECTRE VARIANT 1",
+      #     "CVE": "CVE-2017-5753",
+      #     "VULNERABLE": false,
+      #     "INFOS": "106 opcodes found, which is >= 70, heuristic to be improved when official patches become available"
+      #   },
+      #   {
+      #     "NAME": "SPECTRE VARIANT 2",
+      #     "CVE": "CVE-2017-5715",
+      #     "VULNERABLE": true,
+      #     "INFOS": "IBRS hardware + kernel support OR kernel with retpoline are needed to mitigate the vulnerability"
+      #   },
+      #   {
+      #     "NAME": "MELTDOWN",
+      #     "CVE": "CVE-2017-5754",
+      #     "VULNERABLE": false,
+      #     "INFOS": "PTI mitigates the vulnerability"
+      #   }
+      # ]
+      # EOT
+
+      value = JSON.parse(value)  
+      value.each do | item |
+        # puts item["CVE"]
+        # puts item["VULNERABLE"]
+        Facter.add(item["CVE"]) do
+          setcode do
+            item["VULNERABLE"]
+          end
+        end
+      end
     end
     value
+    # JSON.pretty_generate(value)
 end
