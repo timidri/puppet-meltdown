96171f57f3999f60e08c10c34feb5c475493a8d9
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-01-12 17:04:45 +0100

    Update meltdown_win.rb

--- lib/facter/meltdown_win.rb
+++ lib/facter/meltdown_win.rb
@@ -3,15 +3,11 @@ Facter.add('meltdown') do
     setcode do
       require 'json'
       value = nil
-      powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-      command = '$arrCVE = @{}
-switch -Wildcard ((Get-WmiObject -class Win32_OperatingSystem).Caption) {'Microsoft Windows Server 2008 R2*' { $hotfix = 'KB4056897' } 'Microsoft Windows Server 2012 R2*' { $hotfix = 'KB4056898' } 'Microsoft Windows Server 2016*' { $hotfix = 'KB4056890' } 'Microsoft Windows Server, version 1709*' { $hotfix = 'KB4056892' }}
-if ($hotfix) {$arrCVE.Add('CVE-2017-5715', [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix))
-    $arrCVE.Add('CVE-2017-5753', [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix))
-    $arrCVE.Add('CVE-2017-5754', [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix))}
-$arrCVE | ConvertTo-Json'
-      command_one_line = command.sub("\r\n",";")
-      value = Facter::Core::Execution.execute(%Q{#{powershell} -ExecutionPolicy Remote -command "#{command_one_line}"})
+      sysroot = ENV['SystemRoot']
+      programdata = ENV['ProgramData']
+      powershell = "#{sysroot}\system32\WindowsPowerShell\v1.0\powershell.exe"
+      checker_script = "#{programdata}\PuppetLabs\puppet\cache\lib\spectre-meltdown-checker.ps1"
+      value = Facter::Core::Execution.execute(%Q{#{powershell} -ExecutionPolicy Remote -file "#{checker_script}"})
     end
     value
 end
