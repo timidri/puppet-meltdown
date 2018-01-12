2523d7dd6e25b8d18c2511a9bce666753134b347
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-01-12 16:49:43 +0100

    Update meltdown.rb

--- lib/facter/meltdown.rb
+++ lib/facter/meltdown.rb
@@ -1,10 +1,9 @@
 Facter.add('meltdown') do
-    # confine :kernel => :linux
+    confine :kernel => :Linux
     value = ""
     checker_script = ""
     setcode do
       require 'json'
-    #    value = Facter::Core::Execution.exec('/var/tmp/spectre-meltdown-check --batch json')
       checker_script = "/opt/puppetlabs/puppet/cache/lib/spectre-meltdown-checker.sh"
       value = Facter::Core::Execution.exec("/bin/sh #{checker_script} --batch json")
       value = JSON.pretty_generate(JSON(value))
