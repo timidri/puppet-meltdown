65946f60707eaead568f657614633205ba9be4a5
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-01-12 19:08:49 +0100

    added compliance checks

--- lib/spectre-meltdown-checker.ps1
+++ lib/spectre-meltdown-checker.ps1
@@ -1,4 +1,6 @@
 Import-Module $ENV:ProgramData\PuppetLabs\puppet\cache\lib\SpeculationControl.psd1
+function global:Write-Host() {}
+$SpeculationControl = Get-SpeculationControlSettings
 
 $arrCVE = @{}
 switch -Wildcard ((Get-WmiObject -class Win32_OperatingSystem).Caption) {
@@ -8,8 +10,22 @@ switch -Wildcard ((Get-WmiObject -class Win32_OperatingSystem).Caption) {
     'Microsoft Windows Server, version 1709*' { $hotfix = 'KB4056892' }
 }
 if ($hotfix) {
-    $arrCVE.Add('CVE-2017-5715', [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix))
-    $arrCVE.Add('CVE-2017-5753', [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix))
-    $arrCVE.Add('CVE-2017-5754', [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix))
+    $arrCVE.Add('CVE-2017-5715', @{
+        "hotfix_installed" = $SpeculationControl.BTIWindowsSupportPresent
+        "hotfix_enabled"   = $SpeculationControl.BTIWindowsSupportEnabled
+        "hardware_support" = $SpeculationControl.BTIHardwarePresent
+        "compliant"        = if ($SpeculationControl.BTIWindowsSupportEnabled -and $SpeculationControl.BTIHardwarePresent) {$True} else {$False}
+    })
+    $arrCVE.Add('CVE-2017-5753', @{
+        "hotfix_installed" = [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix)
+        "compliant"        = if ([bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix)) {$True} else {$False}
+    })
+    $arrCVE.Add('CVE-2017-5754', @{
+        "hotfix_installed" = $SpeculationControl.KVAShadowWindowsSupportPresent
+        "hotfix_enabled"   = $SpeculationControl.KVAShadowWindowsSupportEnabled
+        "hardware_support" = $SpeculationControl.KVAShadowPcidEnabled
+        "hardware_support_required" = $SpeculationControl.KVAShadowRequired
+        "compliant"        = if (!$SpeculationControl.KVAShadowRequired -or ($SpeculationControl.KVAShadowRequired -and $SpeculationControl.KVAShadowWindowsSupportEnabled)) {$True} else {$False}
+    })
 }
 $arrCVE | ConvertTo-Json
