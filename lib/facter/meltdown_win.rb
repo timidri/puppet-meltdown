bfe706555ef7921a9daf9fa90047f5a6bfdd1cac
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-01-12 18:00:30 +0100

    Update meltdown_win.rb

--- lib/facter/meltdown_win.rb
+++ lib/facter/meltdown_win.rb
@@ -1,14 +1,10 @@
 Facter.add('meltdown') do
     confine :osfamily => :windows
     setcode do
-        require 'json'
-        value = nil
         sysroot = ENV['SystemRoot']
         programdata = ENV['ProgramData']
         powershell = "C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
         checker_script = "C:\\ProgramData\\PuppetLabs\\puppet\\cache\\lib\\spectre-meltdown-checker.ps1"
-        value = Facter::Core::Execution.execute("#{powershell} -ExecutionPolicy Unrestricted -File #{checker_script}")
-        value = JSON.pretty_generate(JSON(value))
+        Facter::Util::Resolution.execute("#{powershell} -ExecutionPolicy Unrestricted -File #{checker_script}")
     end
-    value
 end
