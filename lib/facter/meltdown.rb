4121f0168cd96bd48596fa1082cb51e99b0ca540
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2018-01-12 12:51:35 +0100

    added testability

--- lib/facter/meltdown.rb
+++ lib/facter/meltdown.rb
@@ -1,42 +1,51 @@
+require 'json'
+
+def get_json
+  value = <<-EOT
+  [
+    {
+      "NAME": "SPECTRE VARIANT 1",
+      "CVE": "CVE-2017-5753",
+      "VULNERABLE": false,
+      "INFOS": "106 opcodes found, which is >= 70, heuristic to be improved when official patches become available"
+    },
+    {
+      "NAME": "SPECTRE VARIANT 2",
+      "CVE": "CVE-2017-5715",
+      "VULNERABLE": true,
+      "INFOS": "IBRS hardware + kernel support OR kernel with retpoline are needed to mitigate the vulnerability"
+    },
+    {
+      "NAME": "MELTDOWN",
+      "CVE": "CVE-2017-5754",
+      "VULNERABLE": false,
+      "INFOS": "PTI mitigates the vulnerability"
+    }
+  ]
+  EOT
+  value
+end
+
 Facter.add('meltdown') do
     # confine :kernel => :linux
     value = ""
     checker_script = ""
     setcode do
-      require 'json'
-      # get the script path relative to facter Ruby program
-      checker_script = File.join(File.expand_path(File.dirname(__FILE__)), '..', 
-        'meltdown', 'spectre-meltdown-checker.sh')
-      value = JSON(Facter::Core::Execution.exec("/bin/sh #{checker_script} --batch json"))
-      # value = <<-EOT
-      # [
-      #   {
-      #     "NAME": "SPECTRE VARIANT 1",
-      #     "CVE": "CVE-2017-5753",
-      #     "VULNERABLE": false,
-      #     "INFOS": "106 opcodes found, which is >= 70, heuristic to be improved when official patches become available"
-      #   },
-      #   {
-      #     "NAME": "SPECTRE VARIANT 2",
-      #     "CVE": "CVE-2017-5715",
-      #     "VULNERABLE": true,
-      #     "INFOS": "IBRS hardware + kernel support OR kernel with retpoline are needed to mitigate the vulnerability"
-      #   },
-      #   {
-      #     "NAME": "MELTDOWN",
-      #     "CVE": "CVE-2017-5754",
-      #     "VULNERABLE": false,
-      #     "INFOS": "PTI mitigates the vulnerability"
-      #   }
-      # ]
-      # EOT
-
-    value
+      os_family = Facter.value(:osfamily)
+      if Facter.value(:osfamily) == 'Darwin'
+        value = JSON.parse(get_json)
+      else
+        # get the script path relative to facter Ruby program
+        checker_script = File.join(File.expand_path(File.dirname(__FILE__)), '..', 
+          'meltdown', 'spectre-meltdown-checker.sh')
+        value = JSON.parse(Facter::Core::Execution.exec("/bin/sh #{checker_script} --batch json"))
+      end
+    JSON.pretty_generate(value)
     end
 end
 
 # generate CVE-specific facts
-meltdown_hash = JSON(Facter.value('meltdown'))
+meltdown_hash = JSON.parse(Facter.value('meltdown'))
 
 meltdown_hash.each do | item |
   # puts item["CVE"].downcase
