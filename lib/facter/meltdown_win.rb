a69a0d5e9312ed20a6ea16107ced65f52f16e3e3
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-01-12 18:10:43 +0100

    Update meltdown_win.rb

--- lib/facter/meltdown_win.rb
+++ lib/facter/meltdown_win.rb
@@ -5,6 +5,6 @@ Facter.add('meltdown') do
         programdata = ENV['ProgramData']
         powershell = "C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
         checker_script = "C:\\ProgramData\\PuppetLabs\\puppet\\cache\\lib\\spectre-meltdown-checker.ps1"
-        Facter::Util::Resolution.execute("#{powershell} -ExecutionPolicy Unrestricted -File #{checker_script}")
+        Facter::Util::Resolution.exec("#{powershell} -ExecutionPolicy Unrestricted -File #{checker_script}")
     end
 end
