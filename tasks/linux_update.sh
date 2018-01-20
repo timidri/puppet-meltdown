510e0b8fd3f3d46acbbe6903bd3e16014f63e103
Dimitri Tischenko <dimitri@puppet.com>
2018-01-20 21:55:41 +0100

    removing comments and echos

--- tasks/linux_update.sh
+++ tasks/linux_update.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-# kernel_update
-#
-# This is where you put the shell code for your task.
-#
-# You can write Puppet tasks in any language you want and it's easy to 
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# http://puppet.com/docs/bolt/latest/converting_scripts_to_tasks.html 
-# 
-# Puppet tasks make it easy for you to enable others to use your script. Tasks 
-# describe what it does, explains parameters and which are required or optional, 
-# as well as validates parameter type. For examples, if parameter "instances" 
-# must be an integer and the optional "datacenter" parameter must be one of 
-# portland, sydney, belfast or singapore then the .json file 
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     }, 
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/latest/task_metadata.html
-#
-
 update_redhat() {
-    echo "PT_force: ${PT_force}"
+    # echo "PT_force: ${PT_force}"
     yum_options='--assumeno'
     if [ "${PT_force}" == "true" ] ; then
         yum_options='--assumeyes'
@@ -38,7 +11,7 @@ update_redhat() {
 }
 
 update_debian() {
-    echo "PT_force: ${PT_force}"
+    # echo "PT_force: ${PT_force}"
     apt_options='--assume-no'
     if [ "${PT_force}" == "true" ] ; then
         apt_options='--assume-yes'
