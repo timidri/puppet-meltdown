968dd3478328afbb6f106073e0c4973199955263
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-06-06 23:54:08 +0200

    Removed reliance on HotfixID's

--- lib/meltdown/spectre-meltdown-checker.ps1
+++ lib/meltdown/spectre-meltdown-checker.ps1
@@ -3,26 +3,14 @@ Function Add-SpectreVariants {
     Import-Module $ENV:ProgramData\PuppetLabs\puppet\cache\lib\meltdown\SpeculationControl.psd1
     function global:Write-Host() {}
     $SpeculationControl = Get-SpeculationControlSettings
-    
-    switch -Wildcard ((Get-WmiObject -class Win32_OperatingSystem).Caption) {
-        'Microsoft Windows Server 2008 Standard*'   { $hotfix = 'KB4090450' }
-        'Microsoft Windows Server 2008 Enterprise*' { $hotfix = 'KB4090450' }
-        'Microsoft Windows Server 2008 Datacenter'  { $hotfix = 'KB4090450' }
-        'Microsoft Windows Server 2008 R2*'         { $hotfix = 'KB4056897' }
-        'Microsoft Windows Server 2012 Standard*'   { $hotfix = 'KB4088877' }
-        'Microsoft Windows Server 2012 Datacenter*' { $hotfix = 'KB4088877' }
-        'Microsoft Windows Server 2012 R2*'         { $hotfix = 'KB4056898' }
-        'Microsoft Windows Server 2016*'            { $hotfix = 'KB4056890' }
-        'Microsoft Windows Server, version 1709*'   { $hotfix = 'KB4056892' }
-    }
 
-    if ($hotfix) {
+    if ([Environment]::OSVersion.Version.Major -ge 6) {
         $arrCVE.Add('CVE-2017-5753', @{
             "CVE"              = "2017-5753"
             "description"      = "Spectre Variant 1"
-            "vulnerable"       = if ([bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix)) {$False} else {$True}
+            "vulnerable"       = !$SpeculationControl.BTIWindowsSupportPresent
             "info"             = @{
-                "hotfix_installed" = [bool](Get-WmiObject -query 'select * from win32_quickfixengineering' | ? HotFixID -eq $hotfix)
+                "hotfix_installed" = $SpeculationControl.BTIWindowsSupportPresent
             }
         })
         $arrCVE.Add('CVE-2017-5715', @{
