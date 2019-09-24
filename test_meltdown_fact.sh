0a7e2b5b1289e41375bb8d21beeb63f1e700db32
Dimitri Tischenko <dimitri@puppet.com>
2019-09-24 15:13:40 +0100

    fixed bolt command, added echo statements

--- test_meltdown_fact.sh
+++ test_meltdown_fact.sh
@@ -5,13 +5,17 @@ scope=${1:-"test"}
 # install gems
 bundle install
 # provision nodes for testing scope
+echo "Provisioning scope ${scope}..."
 bundle exec rake "litmus:provision_list[${scope}]"
-[ $scope == "travis" ] && bolt task run package name=curl action=install --nodes all --modulepath=spec/fixtures/modules --inventory=./inventory.yaml
+[ $scope == "travis" ] && bundle exec bolt task run package name=curl action=install --nodes all --modulepath=spec/fixtures/modules --inventory=./inventory.yaml
 # install puppet agent on the nodes
+echo "Installing agent..."
 bundle exec rake litmus:install_agent
 # install the meltdown module on the nodes
+echo "Installing meltdown module..."
 bundle exec rake litmus:install_module
 # run the acceptance tests in parallel
+echo "Running acceptance tests..."
 bundle exec rake litmus:acceptance:parallel
 if [ $? == 0 ]; then
   # tear down the nodes. Note: if the testing failed,
